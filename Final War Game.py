7f6e3fb2c582ae09f033d815b92ec5fa7eb4cce6
hcasnocha <89221733+hcasnocha@users.noreply.github.com>
2022-04-21 14:39:29 -0400

    Create Basic GUI
    I wrote the code for the basic frame and layout of the GUI with labels and buttons.

--- Final War Game.py
+++ Final War Game.py
@@ -7,8 +7,29 @@ root.iconbitmap()
 root.geometry("900x500")
 root.configure(background="green")
 
+my_frame = Frame(root, bg="green")
+my_frame.pack(pady=20)
 
+# Frames for Cards
+dealer_frame = LabelFrame(my_frame, text="dealer", bd=0)
+dealer_frame.grid(row=0, column=0, padx=20, ipadx=20)
 
+player_frame = LabelFrame(my_frame, text="player", bd=0)
+player_frame.grid(row=0, column=1, ipadx=20)
+
+# Put Cards in Frames
+dealer_label = Label(dealer_frame, text='')
+dealer_label.pack(pady=20)
+
+player_label = Label(player_frame, text='')
+player_label.pack(pady=20)
+
+# Create Buttons
+shuffle_button = Button(root, text="Shuffle Deck", font=("Helvetica", 14))
+shuffle_button.pack(pady=20)
+
+card_button = Button(root, text="Get Cards", font=("Helvetica", 14))
+card_button.pack(pady=20)
 
 
 root.mainloop()
